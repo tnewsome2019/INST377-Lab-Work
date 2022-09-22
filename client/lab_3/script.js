822a58c7cd0f50359e984a54688bfe14d6de8dbd
Trinity Newsome <tnewsome2019@gmail.com>
2022-09-21 21:38:52 -0400

    js changes

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -19,6 +19,13 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  }); 
+
+  slides[slidePosition].classList.add('visible'); 
+
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
