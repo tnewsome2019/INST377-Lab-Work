6413380c7d999d4d843520c2df86339b171b320a
Trinity Newsome <tnewsome2019@gmail.com>
2022-09-21 22:03:44 -0400

    slide and button changes

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -22,9 +22,10 @@ function updateSlidePosition() {
   slidesArray.forEach((slide) => {
     slide.classList.remove('visible');
     slide.classList.add('hidden');
-  }); 
+  });
 
-  slides[slidePosition].classList.add('visible'); 
+  console.log(slidePosition);
+  slides[slidePosition].classList.add('visible');
 
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
@@ -42,6 +43,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -52,6 +58,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition -= 1;
+  }
   updateSlidePosition();
 }
 
