fda09c908649fe62584e753a9796bddeac4f45a1
Trinity Newsome <tnewsome2019@gmail.com>
2022-09-29 15:55:30 -0400

    tetromino movement

--- tetris/script.js
+++ tetris/script.js
@@ -43,5 +43,65 @@ document.addEventListener('DOMContentLoaded', () => {
         [width, width+1, width+2, width+3]
     ]
     
+    const theTetrominos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
-})
+    let currentPosition = 4
+    let currentRotation = 0
+    
+
+    //randomly select a Tetromino and its rotation
+    let random = Math.floor(Math.random()*theTetrominos.length)
+    let current = theTetrominos[random][0]
+
+    //draw the tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+    
+    //undraw the Tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    //make the tetromino move down every second 
+    timerId = setInterval(moveDown, 1000)
+
+    //move dwon function
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+    }
+
+    //freeze function
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //start new tetrmino falling
+            random = Math.floor(Math.random() * theTetrominos.length)
+            current = theTetrominos[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //move lfet unless theres a blockage 
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -+1
+
+        if(current.some(index => squares[currentPosition+ index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    })
